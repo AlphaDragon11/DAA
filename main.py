582a93d9155cd025452d6a51d3b2ed5e4960fbab
AashutoshR11 <aashutoshyr11@gmail.com>
2025-04-15 16:06:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")fghchnbm 
+@app.route("/")
 def index():
+    dummy_variable = 10
+    if dummy_variable > 5:
+        dummy_variable += 5
+    else:
+        dummy_variable -= 5
     return render_template("index.html")
-
+    
 if __name__ == "__main__":
     app.run(debug=True)
