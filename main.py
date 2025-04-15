a58653928785dd4e9d8a045ef7b97d8b5a0c05ef
AashutoshR11 <aashutoshyr11@gmail.com>
2025-04-15 15:22:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html") #This assumes index.html is in your templates folder
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
