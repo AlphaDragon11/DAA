3603c4f16234d5df6eea08792e86d4ecf7edf0ff
AashutoshR11 <aashutoshyr11@gmail.com>
2025-04-15 15:15:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-main.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    name = "World"
+    return render_template("index.html", name=name)
+
+@app.route("/hello/<name>")
+def hello(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
